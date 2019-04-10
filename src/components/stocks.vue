4a8657131efb80f205fd208d1239076e81d1abb5
David Churn <dchurn@me.com>
2019-04-10 17:20:13 -0500

    started working on stocks component

--- src/components/stocks.vue
+++ src/components/stocks.vue
@@ -1,22 +1,52 @@
 <template>
   <div class="stocks">
-    <h3>{{title}}</h3>
+    <h3 class="row">
+      <div class="col-amount">{{ subtotalAmt }}</div>
+      <div class="flex10">{{ title }}</div>
+    </h3>
     <div class="row">
       <div class="col-amount">Balance</div>
-      <div class="flex1">Shares</div>
-      <div class="flex1">Price</div>
+      <div class="col-amount">Shares</div>
       <div class="flex1">Symbol</div>
-      <div class="flex1">Description</div>
-      <div class="flex1">Annual Dividends</div>
+      <div class="flex4">Description</div>
+      <div class="col-amount">Annual Dividends</div>
+      <div class="flex1">Action</div>
     </div>
-    <div class="row"
-      v-for="(asset,index) in allStocks" :key="index">
-      <div class="col-amount"><input v-model="asset.amount" type="number" step=".01" class="amt"></div>
-      <div class="flex1"><input v-model="asset.shares" type="number" step=".000001" class="amt"></div>
-      <div class="flex1">{{ asset.price ? asset.price.toDecFormat(2) : Number(0).toFixed(2) }}</div>
-      <div class="flex1"><input v-model="asset.symbol" type="text"></div>
-      <div class="flex1"><input v-model="asset.description" type="text"></div>
-      <div class="flex1"><input v-model="asset.payment" type="number" step=".01" class="amt"></div>
+    <div class="row">
+      <div class="flex2"></div>
+      <div class="col-amount">Price</div>
+      <div class="flex8">Company Name</div>
+    </div>
+    <div
+      v-for="(asset,index) in stocksAssets" :key="index">
+      <div class="row">
+        <div class="col-amount">{{ asset.shares * lookup.price }}</div>
+        <div class="col-amount">
+          <input type="number" step=".000001" class="amt"
+            :value="asset.shares"
+            @focusout="updateRow($event,asset,'shares')">
+        </div>
+        <div class="flex1">
+          <input type="text"
+            :value="asset.symbol"
+            @focusout="updateRow($event,asset,'symbol')">
+        </div>
+        <div class="flex4">
+          <input type="text"
+             :value="asset.description"
+             @focusout="updateRow($event,asset,'description')">
+        </div>
+        <div class="col-amount">
+          <input type="number" step=".01" class="amt"
+            :value="asset.payment"
+            @focusout="updateRow($event,asset,'payment')">
+        </div>
+      </div>
+      <div class="row">
+        <div class="flex2"></div>
+        <div class="col-amount">{{ asset.price ? asset.price.toDecFormat(2) : Number(0).toFixed(2) }}</div>
+        <div class="flex8">{{ asset.company }}</div>
+      </div>
     </div>
   </div>
 </template>
