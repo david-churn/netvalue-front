509c21c9c83807be3b2a3f8c1d3525005eff416e
David Churn <dchurn@me.com>
2019-05-06 20:28:18 -0500

    authorization

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+'use strict'
+// 4/12/2019 David Churn recreated
+
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBcYPGbpT3k777v3zdGeMF1aaKKFr_KK8M",
+  authDomain: "netvalue-9d083.firebaseapp.com",
+  databaseURL: "https://netvalue-9d083.firebaseio.com",
+  projectId: "netvalue-9d083",
+  storageBucket: "netvalue-9d083.appspot.com",
+  messagingSenderId: "734823607293"
+};
+firebase.initializeApp(config);
